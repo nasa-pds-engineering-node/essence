f03c363ed09ee6445e3b00d11cd8a1bbb0a3988d
Sean Kelly <kelly@seankelly.biz>
2024-03-14 14:24:33 -0500

    doc test

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+/** Hello, this is the App */
 function App() {
   const [count, setCount] = useState(0)
 
